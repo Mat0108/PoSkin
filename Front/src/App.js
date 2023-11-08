b5d0899738bc67cc44cb8d421ac0df4a0106d2aa
Mat0108 <61587759+Mat0108@users.noreply.github.com>
2023-11-08 15:09:22 +0100

    update login et register figma

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import {Route,Routes} from 'react-router';
 
@@ -19,14 +19,47 @@ import Login from './pages/Login';
 import { register } from './services/user';
 import { login } from './services/user';
 import Register from './pages/Register';
+import Modal from 'react-modal';
 
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [divModal,setDivModal] = useState(<></>)
+
+  const customStyles = {
+
+    
+    overlay: {zIndex: 1000}
+  };
+  
+  const openModal = (element,test) => {
+    console.log(test)
+    setIsModalOpen(true);
+    setDivModal(element);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setDivModal(<></>)
+  };
+  let register = <Register type={false} close={()=>closeModal()} login={()=>{}}/>
+  let login = <Login type={true} close={()=>closeModal()} register={()=>{openModal(register)}}/>
+  register = <Register type={false} close={()=>closeModal()} login={()=>{openModal(login,"test")}}/>
+  login = <Login type={true} close={()=>closeModal()} register={()=>{openModal(register,"test")}}/>
   return (
     <div className="App w-full h-full relative bg-[#EEE8E4] font-mt">
       <Router>
-        <Navbar />
+        <Navbar Login={()=>openModal(login)} Register={()=>openModal(register)}/>
         <ScrollToTop />
+
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={closeModal}
+          className={"bg-transparent w-screen h-screen z-[10000] flex center"}
+          style={customStyles}
+        >
+          {divModal}
+        </Modal>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
           {/* <Route path="/Expertise" element={<Expertise/>}></Route> 
