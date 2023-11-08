221c2a40d178cb35e109f3e7f1cd979b95db9ca7
Mat0108 <61587759+Mat0108@users.noreply.github.com>
2023-11-08 09:58:46 +0100

    update config

--- Front/src/services/config.js
+++ Front/src/services/config.js
@@ -1,3 +1,6 @@
 const urlProd = "https://poapi1-sgl9cp2c.b4a.run";
+const urlPreProd = "https://popreprod1-6ypw0azz.b4a.run"
 const urlDev = "https://poapimatthieu-wi4dzk4k.b4a.run"
-export const url = urlProd;
+const urlMatthieu = "https://poapimatthieu1-kxe2gxq6.b4a.run"
+const urlCoumba = "https://poapicoumba1-k4tjbp5a.b4a.run"
+export const url = urlDev;
